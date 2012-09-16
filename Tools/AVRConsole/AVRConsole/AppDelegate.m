e8e1f56104fdf499c22f529ca8e780bb765399e7
Jeff Hutchison <jeff@jeffhutchison.com>
2012-09-16 08:00:46 -0400

    Add Cocoa app project skeleton.

---
+++ Tools/AVRConsole/AVRConsole/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  AVRConsole
+//
+//  Created by Jeff Hutchison on 9/16/12.
+//  Copyright (c) 2012 Jeff Hutchison. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
